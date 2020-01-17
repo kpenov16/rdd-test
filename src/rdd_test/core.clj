c5f5b1e9cb57396902bafa2e5fe05e269bdee810
Kaloyan Penov <s133967@student.dtu.dk>
2020-01-17 08:31:55 +0100

    make get! work, status: to be tested and refactored

--- src/rdd_test/core.clj
+++ src/rdd_test/core.clj
@@ -24,35 +24,6 @@
   (match? myFormalField 5))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Template impl ;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defprotocol ITemplate
-  (match-it [thisTemplate otherTemplate]))
-
-(defrecord Template [templateFields]
-  ITemplate
-  (match-it [thisTemplate otherTemplate]
-    (if (= (count (:templateFields thisTemplate)) (count (:templateFields otherTemplate)))
-      (not (some false? (map #(match? %1 (:v %2) ) (:templateFields thisTemplate) (:templateFields otherTemplate))))
-      false)))
-
-(defn new-Template [& args]
-  (if (nil? args)
-    (throw (NullPointerException. "The value passed to Template cannot be nil"))
-    (let [fields (if (vector? args) args (vec args))]
-      (->Template
-        (into [] (for [f fields]
-                   (if (satisfies? TemplateField f) f (new-ActualField f))))))))
-
-
-(comment
-  (def myTemplate (new-Template 2 5))
-  (def myTemplate (new-Template 5))
-  (def myTemplate (new-Template (new-FormalField 5) (new-FormalField "hi"))))
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ActualField impl ;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defrecord ActualField [v]
@@ -71,6 +42,32 @@
   (def myActualField (new-ActualField 5))
   (match? myActualField 5))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Template impl ;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defprotocol ITemplate
+  (match-it [thisTemplate otherTemplate]))
+
+(defrecord Template [templateFields]
+  ITemplate
+  (match-it [thisTemplate otherTemplate]
+    (if (= (count (:templateFields thisTemplate)) (count (:templateFields otherTemplate)))
+      (not (some false? (map #(match? %1 (:v %2) ) (:templateFields otherTemplate) (:templateFields thisTemplate))))
+      false)))
+
+(defn new-Template [& args]
+  (if (nil? args)
+    (throw (NullPointerException. "The value passed to Template cannot be nil"))
+    (let [fields (if (vector? args) args (vec args))]
+      (->Template
+        (into [] (for [f fields]
+                   (if (satisfies? TemplateField f) f (new-ActualField f))))))))
+
+
+(comment
+  (def myTemplate (new-Template 2 5))
+  (def myTemplate (new-Template 5))
+  (def myTemplate (new-Template (new-FormalField 5) (new-FormalField "hi"))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -119,9 +116,9 @@
     (not (some false? (map #(match? %1 (:v %2) ) col1 col2)))
     false))
 
+(defn chan-size? [bound] (if (>= 0 bound) 1 bound))
 
-
-(defrecord SequentialSpace [bound tuples put-chan get-chan]
+(defrecord SequentialSpace [bound tuples ret-tupl get!-lock put-chan mult-put-chan]
   Space
   (ssize [space]
     (count (:tuples space)))
@@ -136,32 +133,59 @@
                               old
                               (conj old new)))
                           fields)]
-          (if (= tNew (conj tOld fields))
-            (do
-              (put! put-chan "new-tuple-event")
-              true)
-            false)))))
+          (do
+            (async/>!! put-chan "new-tuple-event")
+            true)))))
 
   (get! [space templateFields]
-    (locking space
+    (locking (:get!-lock space)
       (dosync
         (alter (:tuples space) (fn [tuples templ-fields]
-                                 (for [fields tuples]
-                                   (for [field fields
-                                         to-find-field templ-fields]
-                                     (if (match? to-find-field field)
-                                       true
-                                       false))) templateFields))))))
+                                 (let [tap-put-chan (async/chan (chan-size? bound))
+                                       t- (async/tap (:mult-put-chan space) tap-put-chan)
+                                       for_loop (for [t tuples]
+                                                  (do
+                                                    (println t templ-fields)
+                                                    (match-it t templ-fields)))]
 
+
+                                   (do
+                                     #_(println (doall for_loop))
+                                     (let [pos (.indexOf for_loop true)]
+                                       (if (> pos -1)
+                                           (do
+                                             (ref-set (:ret-tupl space) (tuples pos))
+                                             (vec (concat (subvec tuples 0 pos) (subvec tuples (inc pos)))))
+                                           (do
+                                             (async/<!! tap-put-chan)
+                                             (recur (:tuples space) templ-fields))))
+                                             ;;this recur might be recursive call to get! instead
+                                     #_tuples))) templateFields)
+        (deref (:ret-tupl space))))))
+
+
+#_(if (= tNew (conj tOld fields))
+    (do
+      (put! put-chan "new-tuple-event")
+      true)
+    false)
+(comment
+  (def mySpace1 (new-SequentialSpace 2))
+  (put! mySpace1 (new-Template 1 2))
+  (put! mySpace1 (new-Template "hi" 2))
+  (get! mySpace1 (new-Template (new-ActualField "hi") (new-ActualField 2))))
 
 
 (defn new-SequentialSpace-
   ([bound tuples]
    {:pre [(int? bound) (vector? @tuples)]}
-   (let [b (if (>= 0 bound) -1 bound)
-         put-chan-size (if (= b -1) 1 b)
-         get-chan-size (if (= b -1) 1 b)]
-     (->SequentialSpace b tuples (async/chan put-chan-size) (async/chan get-chan-size)))))
+   (let [ret-tupl (ref nil)
+         get!-lock 0
+         b (if (>= 0 bound) -1 bound)
+         put-chan-size (chan-size? b)
+         put-chan (async/chan put-chan-size)
+         mult-put-chan (async/mult put-chan)]
+     (->SequentialSpace b tuples ret-tupl get!-lock put-chan mult-put-chan))))
 
 (defn new-SequentialSpace
   ([]
