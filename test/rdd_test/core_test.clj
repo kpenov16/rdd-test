db865118157bdb99053682a49f842b8f38f02c05
Kaloyan Penov <s133967@student.dtu.dk>
2020-01-24 21:55:42 +0100

    adding unit tests for FormalField record

--- test/rdd_test/core_test.clj
+++ test/rdd_test/core_test.clj
@@ -2,6 +2,70 @@
   (:require [clojure.test :refer :all]
             [rdd-test.core :refer :all]))
 
+;;;;;;;;;;;;;;;;;;;;;
+;;FormalField tests;;
+;;;;;;;;;;;;;;;;;;;;;
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "givenNewFormalFieldCreatedWithNilValue_returnNullPointerException"
+    (is (thrown? NullPointerException (new-FormalField nil))))
+
+  (testing "givenNewFormalFieldWithString_returnStringInFormalField"
+    (is (= "value" (:v (new-FormalField "value")))))
+
+  (testing "givenNewFormalFieldWithInt_returnIntInFormalField"
+    (is (= 1 (:v (new-FormalField 1)))))
+
+  (testing "givenNewFormalFieldWithSymbol_returnSymbolInFormalField"
+    (is (= :s (:v (new-FormalField :s)))))
+
+  (testing "givenSymbolMatchingTheValueInFormalField_returnTrue"
+    (let [symbol :s
+          formalField (new-FormalField symbol)]
+      (is (match? formalField symbol))))
+
+  (testing "givenSymbolNotMatchingTheValueInFormalField_returnStillTrue"
+    (let [symbol :s
+          formalField (new-FormalField symbol)]
+      (is (match? formalField :otherSymbol))))
+
+  (testing "givenNumberMatchedToSymbolValueInFormalField_returnFalse"
+    (let [symbol :s
+          formalField (new-FormalField symbol)]
+      (is (not (match? formalField 42)))))
+
+  (testing "givenStringMatchedToSymbolValueInFormalField_returnFalse"
+    (let [symbol :s
+          formalField (new-FormalField symbol)]
+      (is (not (match? formalField "42")))))
+
+  (testing "givenNilMatchedToSymbolValueInFormalField_returnFalse"
+    (let [symbol :s
+          formalField (new-FormalField symbol)]
+      (is (not (match? formalField nil)))))
+
+  (testing "givenSymbolMatchedToNumberValueInFormalField_returnFalse"
+    (let [formalField (new-FormalField 42)]
+      (is (not (match? formalField :symbol)))))
+
+  (testing "givenStringMatchedToNumberValueInFormalField_returnFalse"
+    (let [formalField (new-FormalField 42)]
+      (is (not (match? formalField "some_string")))))
+
+  (testing "givenNilMatchedToNumberValueInFormalField_returnFalse"
+    (let [formalField (new-FormalField 42)]
+      (is (not (match? formalField nil)))))
+
+  (testing "givenSymbolMatchedToStringValueInFormalField_returnFalse"
+    (let [formalField (new-FormalField "some_string")]
+      (is (not (match? formalField :symbol)))))
+
+  (testing "givenNumberMatchedToStringValueInFormalField_returnFalse"
+    (let [formalField (new-FormalField "some_string")]
+      (is (not (match? formalField 42)))))
+
+  (testing "givenNilMatchedToStringValueInFormalField_returnFalse"
+    (let [formalField (new-FormalField "some_string")]
+      (is (not (match? formalField nil))))))
+
+
+(run-tests)
