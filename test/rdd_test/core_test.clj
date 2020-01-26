9a942f729c99bbb46ef65cd50363b2e1e9fcd721
Kaloyan Penov <s133967@student.dtu.dk>
2020-01-26 22:56:04 +0100

    adding tests for ActualField impl

--- test/rdd_test/core_test.clj
+++ test/rdd_test/core_test.clj
@@ -3,9 +3,97 @@
             [rdd-test.core :refer :all]))
 
 ;;;;;;;;;;;;;;;;;;;;;
+;;ActualField tests;;
+;;;;;;;;;;;;;;;;;;;;;
+(deftest ActualField-tests
+  (testing "givenNewActualFieldCreatedWithNilValue_returnNullPointerException"
+    (is (thrown? NullPointerException (new-ActualField nil))))
+
+  (testing "givenNewActualFieldWithStringValue_returnStringValueInActualField"
+    (let [stringValue "stringValue"
+          emptyString ""]
+      (is (= stringValue (:v (new-ActualField stringValue))))
+      (is (= emptyString (:v (new-ActualField emptyString))))))
+
+
+  (testing "givenNewActualFieldWithIntValue_returnIntValueInActualField"
+    (let [negativeInt -42
+          zeroInt 0
+          positiveInt 42]
+      (is (= negativeInt (:v (new-ActualField negativeInt))))
+      (is (= zeroInt (:v (new-ActualField zeroInt))))
+      (is (= positiveInt (:v (new-ActualField positiveInt))))))
+
+  (testing "givenNewActualFieldWithVector_returnVectorInActualField"
+    (is (= [1 2 :s "p"] (:v (new-ActualField [1 2 :s "p"])))))
+
+  (testing "givenNewActualFieldWithMap_returnMapInActualField"
+    (is (= {:p 42 42 :p "pi" 3.14 3.14 "pi"} (:v (new-ActualField {:p 42 42 :p "pi" 3.14 3.14 "pi"})))))
+
+  (testing "givenNewActualFieldWithSet_returnSetInActualField"
+    (is (= #{:p 42 "pi" 3.14} (:v (new-ActualField #{:p 42 "pi" 3.14})))))
+
+  (testing "givenNewActualFieldWithSymbolValue_returnSymbolValueInActualField"
+    (is (= :someSymbol (:v (new-ActualField :someSymbol)))))
+
+  (testing "givenSymbolMatchingTheValueInActualField_returnTrue"
+    (let [symbol :s
+          actualField (new-ActualField symbol)]
+      (is (match? actualField symbol))))
+
+  (testing "givenSymbolNotMatchingTheValueInActualField_returnFalse"
+    (let [symbol :s
+          actualField (new-ActualField symbol)]
+      (is (not (match? actualField :otherSymbol)))))
+
+  (testing "givenNumberMatchedToSymbolValueInActualField_returnFalse"
+    (let [symbol :s
+          actualField (new-ActualField symbol)]
+      (is (not (match? actualField 42)))))
+
+  (testing "givenStringMatchedToSymbolValueInActualField_returnFalse"
+    (let [symbol :s
+          actualField (new-ActualField symbol)]
+      (is (not (match? actualField "42")))))
+
+  (testing "givenNilMatchedToSymbolValueInActualField_returnFalse"
+    (let [symbol :s
+          actualField (new-ActualField symbol)]
+      (is (not (match? actualField nil)))))
+
+  (testing "givenSymbolMatchedToNumberValueInActualField_returnFalse"
+    (let [actualField (new-ActualField 42)]
+      (is (not (match? actualField :symbol)))))
+
+  (testing "givenStringMatchedToNumberValueInActualField_returnFalse"
+    (let [actualField (new-ActualField 42)]
+      (is (not (match? actualField "some_string")))))
+
+  (testing "givenNilMatchedToNumberValueInActualField_returnFalse"
+    (let [actualField (new-ActualField 42)]
+      (is (not (match? actualField nil)))))
+
+  (testing "givenSymbolMatchedToStringValueInActualField_returnFalse"
+    (let [actualField (new-ActualField "some_string")]
+      (is (not (match? actualField :symbol)))))
+
+  (testing "givenNumberMatchedToStringValueInActualField_returnFalse"
+    (let [actualField (new-ActualField "some_string")]
+      (is (not (match? actualField 42)))))
+
+  (testing "givenNilMatchedToStringValueInActualField_returnFalse"
+    (let [actualField (new-ActualField "some_string")]
+      (is (not (match? actualField nil))))))
+
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;
 ;;FormalField tests;;
 ;;;;;;;;;;;;;;;;;;;;;
-(deftest a-test
+(deftest FormalField-tests
   (testing "givenNewFormalFieldCreatedWithNilValue_returnNullPointerException"
     (is (thrown? NullPointerException (new-FormalField nil))))
 
